f4d23cb7b71da74d069a294080b2c27be78a9e37
Jairo Aguirre <vanderjak@protonmail.com>
2021-07-29 22:13:35 -0700

    Use conditionals about wheather

---
+++ conditionals.js
@@ -0,0 +1,34 @@
+const raining = true;
+const cold = false;
+
+// Part 1
+/*if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");*/
+
+// Part 2
+/*const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}*/
+
+// Part 3
+const temperature = 22;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
